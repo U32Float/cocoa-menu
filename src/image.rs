f131b332beb8e5a1b8ad2c107328a8767f1293b1
U32Float <123883732+U32Float@users.noreply.github.com>
2023-08-13 15:00:20 +0200

    Update image.rs

--- src/image.rs
+++ src/image.rs
@@ -1,7 +1,6 @@
 use objc::runtime::Object;
 use objc::sel_impl;
 use objc::{class, msg_send};
-use objc_foundation::{INSData, NSData};
 use objc_id::{Id, Shared};
 
 use crate::id;
@@ -13,10 +12,17 @@ pub struct Image(pub(crate) Id<Object, Shared>);
 
 impl Image {
     pub fn from_raw(bytes: &[u8]) -> Self {
-        let data = NSData::with_bytes(bytes);
+        let cls = class!(NSData);
+        let data: id = unsafe {
+            let obj: id = msg_send![cls, alloc];
+            let obj: id = msg_send![obj, initWithBytes:bytes.as_ptr()
+                                                       length:bytes.len()];
+            obj
+        };
         let obj: Id<Object, Shared> = unsafe {
             let alloc: id = msg_send![class!(NSImage), alloc];
             let obj = Id::from_retained_ptr(msg_send![alloc, initWithData: data]);
+            let _: () = msg_send![data, release];
             obj
         };
 
